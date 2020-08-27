e36682fa9dae822fec05e9af8c81e09ed22e9ca1
dola-basafi <dola.doit@gmail.com>
2020-08-27 12:53:48 +0700

    deploy heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 
 
